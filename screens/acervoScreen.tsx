3ffae50dc3a6a0e37a6fe7c9a4b2047603b13f9a
JoiceMelo <87379245+JoiceMelo@users.noreply.github.com>
2024-06-23 21:41:02 -0300

    Add files via upload

---
+++ screens/acervoScreen.tsx
@@ -0,0 +1,139 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Ionicons } from '@expo/vector-icons';
+import CustomModal from '../components/customModal';
+
+const Acervo = () => {
+    const navigation = useNavigation();
+    const route = useRoute
+
+    const [modalVisible, setModalVisible] = useState(false);
+    const [user, setUser] = useState<{ firstName: string, lastName: string, email: string } | null>(null);
+
+    const handleAddBook = () => {
+        navigation.navigate('Coleções');
+    };
+
+    const handleAddCollection = () => {
+        navigation.navigate('AddCollectionScreen');
+    };
+
+    const handleSettings = () => {
+        navigation.navigate('UserEdition');
+        setModalVisible(false);
+    };
+
+    const handleLogout = async () => {
+        try {
+            await AsyncStorage.clear();
+            navigation.navigate('Login');
+        } catch (error) {
+            console.log('Error during logout:', error)
+        }
+    }
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            const email = route.params?.email;
+            if (email) {
+                const storedUser = await AsyncStorage.getItem(email);
+                if (storedUser) {
+                    setUser(JSON.parse(storedUser));
+                }
+            }
+        };
+        fetchUser();
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <View style={styles.titleContainer}>
+                    <Image
+                        source={require("../assets/images/images-project/bibliotech.png")}
+                        style={styles.logo}
+                    />
+                    <Text style={styles.pageTitle}>Bibliotech</Text>
+                </View>
+                <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.iconButton}>
+                    <Ionicons name="person-circle-outline" size={30} color="black" />
+                </TouchableOpacity>
+            </View>
+            <Image
+                source={require('../assets/images/images-project/empty.png')}
+                style={styles.image}
+            />
+            <Text style={styles.message}>Comece sua coleção!</Text>
+            <TouchableOpacity style={styles.button} onPress={handleAddCollection}>
+                <Text style={styles.buttonText}>Criar nova coleção</Text>
+            </TouchableOpacity>
+
+            <CustomModal
+                modalVisible={modalVisible}
+                setModalVisible={setModalVisible}
+                user={user}
+                handleSettings={handleSettings}
+                handleLogout={handleLogout}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#be7abb",
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 16,
+    },
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width: '100%',
+        marginBottom: 60,
+        marginTop: -120
+    },
+    titleContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    logo: {
+        width: 45,
+        height: 45,
+        marginRight: 10,
+    },
+    pageTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#000000',
+    },
+    iconButton: {
+        padding: 10,
+    },
+    image: {
+        width: 150,
+        height: 150,
+    },
+    message: {
+        fontSize: 25,
+        color: "#000",
+        marginVertical: 20,
+    },
+    button: {
+        backgroundColor: "#ac58aa",
+        padding: 15,
+        borderRadius: 10,
+        alignItems: "center",
+        width: 200,
+    },
+    buttonText: {
+        color: "#fff",
+        fontSize: 16,
+    },
+});
+
+export default Acervo;
---
+++ screens/editBookScreen.tsx
@@ -0,0 +1,248 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Image, Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { CheckBox } from 'react-native-elements';
+
+const AddCollectionScreen = () => {
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [year, setYear] = useState('');
+    const [edition, setEdition] = useState('');
+    const [pages, setPages] = useState('');
+    const [isbn, setIsbn] = useState('');
+    const [description, setDescription] = useState('');
+    const [status, setStatus] = useState('');
+    const [categories, setCategories] = useState({
+        fiction: false,
+        juvenile: false,
+        religion: false,
+        arts: false,
+        education: false,
+    });
+
+    const navigation = useNavigation();
+
+    const handleCreateBook = async () => {
+        if (title && author && year && edition && pages && isbn && description && status) {
+            const newBook = { title, author, year, edition, pages, isbn, description, status, categories };
+            try {
+                const existingBooks = await AsyncStorage.getItem('collections');
+                const books = existingBooks ? JSON.parse(existingBooks) : [];
+                books.push(newBook);
+                await AsyncStorage.setItem('collections', JSON.stringify(books));
+                Alert.alert('Sucesso', 'Livro adicionado com sucesso!', [{ text: 'OK', onPress: () => navigation.navigate('Coleções') }]);
+            } catch (error) {
+                console.log('Error saving book:', error);
+            }
+        } else {
+            Alert.alert('Erro', 'Por favor, preencha todos os campos.');
+        }
+    };
+
+    const handleEditBook = () => {
+        // Lógica para criar a coleção
+        console.log('livro editado!');
+    };
+    const handleCancelBook = () => {
+        navigation.goBack();
+    };
+
+    const handleCheckboxChange = (category) => {
+        setCategories({ ...categories, [category]: !categories[category] });
+    };
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image
+                    source={require("../assets/images/images-project/bibliotech.png")}
+                    style={styles.logo}
+                />
+                <Text style={styles.pageTitle}>Bibliotech</Text>
+            </View>
+            <View style={styles.content}>
+                <Text style={styles.label}>Status de Leitura</Text>
+                <View style={styles.radioGroup}>
+                    <TouchableOpacity onPress={() => setStatus('Lido')} style={styles.radioButton}>
+                        <View style={[styles.radioCircle, status === 'Lido' && styles.selectedRadio]} />
+                        <Text style={styles.radioText}>Lido</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => setStatus('Lendo')} style={styles.radioButton}>
+                        <View style={[styles.radioCircle, status === 'Lendo' && styles.selectedRadio]} />
+                        <Text style={styles.radioText}>Lendo</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => setStatus('Não Lido')} style={styles.radioButton}>
+                        <View style={[styles.radioCircle, status === 'Não Lido' && styles.selectedRadio]} />
+                        <Text style={styles.radioText}>Não Lido</Text>
+                    </TouchableOpacity>
+                </View>
+
+                <Text style={styles.label}>Título do Livro</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Digite o título do livro"
+                    value={title}
+                    onChangeText={setTitle}
+                />
+                <Text style={styles.label}>Autor</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Digite o autor"
+                    value={author}
+                    onChangeText={setAuthor}
+                />
+                <Text style={styles.label}>Ano de Publicação</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Digite o ano de publicação"
+                    value={year}
+                    onChangeText={setYear}
+                />
+                <Text style={styles.label}>Edição</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Digite a edição"
+                    value={edition}
+                    onChangeText={setEdition}
+                />
+                <Text style={styles.label}>Nº de Páginas</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Digite o número de páginas"
+                    value={pages}
+                    onChangeText={setPages}
+                />
+                <Text style={styles.label}>ISBN</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Digite o ISBN"
+                    value={isbn}
+                    onChangeText={setIsbn}
+                />
+                <Text style={styles.label}>Categorias</Text>
+                <CheckBox
+                    title='Ficção'
+                    checked={categories.fiction}
+                    onPress={() => handleCheckboxChange('fiction')}
+                />
+                <CheckBox
+                    title='Infantil e Juvenil'
+                    checked={categories.juvenile}
+                    onPress={() => handleCheckboxChange('juvenile')}
+                />
+                <CheckBox
+                    title='Religião e Espiritualidade'
+                    checked={categories.religion}
+                    onPress={() => handleCheckboxChange('religion')}
+                />
+                <CheckBox
+                    title='Artes e Fotografia'
+                    checked={categories.arts}
+                    onPress={() => handleCheckboxChange('arts')}
+                />
+                <CheckBox
+                    title='Educação'
+                    checked={categories.education}
+                    onPress={() => handleCheckboxChange('education')}
+                />
+                <Text style={styles.label}>Descrição</Text>
+                <TextInput
+                    style={[styles.input, { height: 100 }]}
+                    placeholder="Digite a descrição"
+                    value={description}
+                    onChangeText={setDescription}
+                    multiline
+                />
+                <TouchableOpacity style={[styles.button, { backgroundColor: '#ac58aa' }]} onPress={handleEditBook}>
+                    <Text style={styles.buttonText}>Salvar alterações</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.button, { backgroundColor: '#ff6347' }]} onPress={handleCancelBook}>
+                    <Text style={styles.buttonText}>Descartar alterações</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#be7abb",
+        padding: 16,
+    },
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        width: '100%',
+        marginBottom: 20,
+        marginTop: -130
+    },
+    logo: {
+        height: 45,
+        width: 45,
+    },
+    pageTitle: {
+        color: "#000000",
+        fontWeight: "bold",
+        fontSize: 24,
+        marginLeft: 10,
+    },
+    content: {
+        width: '100%',
+        marginTop: 45
+    },
+    label: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
+    input: {
+        width: '100%',
+        height: 50,
+        backgroundColor: "#fff",
+        borderRadius: 8,
+        paddingHorizontal: 16,
+        marginBottom: 20,
+    },
+    button: {
+        padding: 15,
+        borderRadius: 10,
+        alignItems: "center",
+        marginBottom: 10,
+    },
+    buttonText: {
+        color: "#fff",
+        fontSize: 16,
+    },
+    radioGroup: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginBottom: 20,
+    },
+    radioButton: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    radioCircle: {
+        height: 20,
+        width: 20,
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: "#000",
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginRight: 5,
+    },
+    selectedRadio: {
+        backgroundColor: "#000",
+    },
+    radioText: {
+        fontSize: 16,
+    },
+});
+
+export default AddCollectionScreen;
